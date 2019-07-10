10bd6f0de1bcbe7b59895b2fc3a0bbf11d1b0b85
katherinegerot <44932613+katherinegerot@users.noreply.github.com>
2019-07-10 16:07:44 -0500

    Update 1.hs

--- Haskell/1.hs
+++ Haskell/1.hs
@@ -1,1 +1,4 @@
-
+main :: IO ()
+main = putStrLn $ "" ++ show (mult 999)
+mult n = foldl1 (\ct x -> ct + x) (rmdups([3,6..n] ++ [5,10..n]))
+rmdups = foldl (\f x -> if x `elem` f then f else f ++ [x]) []
