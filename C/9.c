3dd70b23f0cf96c9ac1f1541069c0bffd75dbf96
katherinegerot <44932613+katherinegerot@users.noreply.github.com>
2019-07-10 14:15:33 -0500

    Streamlined code

--- C/9.c
+++ C/9.c
@@ -9,24 +9,21 @@ int main(void) {
   for(int i = 1; i < 500; i++) {
     for (int j = i + 1; j < 500; j++) {
       for (int k = j + 1; k < 500; k++) {
+        
         if(checkPyt(i, j, k) == 1) {
-          printf("--------- %d --------- \n", i * j * k);
+          printf("%d\n", i * j * k);
         }
+        
       }
     }
-    printf("--------------------\n");
   }
 
   return 0;
 }
 
-
-
 int checkPyt(int a, int b, int c) {
     if (pow(a, 2) + pow(b, 2) == pow(c, 2)) {
-      printf("%d\t\t%d\t\t%d\n", a, b, c);
       if(a + b + c == 1000) {
-        printf("\t\t\t\t\t\t Yay \t\t\t\t\t\t\t\n\n\n\n");
         return 1;
       }
     }
